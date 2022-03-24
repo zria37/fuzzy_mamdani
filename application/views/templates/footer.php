b320a330c4f5c636ab48639d2964d72755e11a5b
Zakharia <zria37@gmail.com>
2022-03-24 21:22:05 +0700

    templating

---
+++ application/views/templates/footer.php
@@ -0,0 +1,29 @@
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="<?php echo base_url('template/plugins/jquery/jquery.min.js')?>"></script>
+<!-- Bootstrap -->
+<script src="<?php echo base_url('template/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+<!-- AdminLTE -->
+<script src="<?php echo base_url('template/plugins/bootstrap/js/bootstrap.bundle.min.js')?>template/dist/js/adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="<?php echo base_url('template/plugins/chart.js/Chart.min.js')?>"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url('template/dist/js/demo.js')?>"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url('template/dist/js/pages/dashboard3.js')?>"></script>
+</body>
+</html>
