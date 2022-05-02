b481462773bd12f8bd87d866d159119e5de910ee
Zakharia <zria37@gmail.com>
2022-05-02 15:39:05 +0700

    testing

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,13 +8,13 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="<?php echo base_url('/template/plugins/fontawesome-free/css/all.min.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url().'/template/plugins/fontawesome-free/css/all.min.css'?>">
   <!-- DataTables -->
-  <link rel="stylesheet" href="<?php echo base_url('template/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')?>">
-  <link rel="stylesheet" href="<?php echo base_url('template/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')?>">
-  <link rel="stylesheet" href="<?php echo base_url('template/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url().'template/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'?>">
+  <link rel="stylesheet" href="<?php echo base_url().'template/plugins/datatables-responsive/css/responsive.bootstrap4.min.css'?>">
+  <link rel="stylesheet" href="<?php echo base_url().'template/plugins/datatables-buttons/css/buttons.bootstrap4.min.css'?>">
   <!-- IonIcons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url('template/dist/css/adminlte.min.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url().'template/dist/css/adminlte.min.css'?>">
 </head>
